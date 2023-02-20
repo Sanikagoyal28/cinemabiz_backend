5eb64b039b42569a0131bd94576fa993c3fc7e15
sanika <sanika2110118@akgec.ac.in>
2023-02-20 18:37:04 +0530

    cors policy access added

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ const port = 4000;
 const DB = "mongodb+srv://sanika:abcd1234@cluster0.kxfedro.mongodb.net/cinemabiz";
 const app = express();
 app.use(express.json())
-app.use(cors({origin:true}))
+app.use(cors({ origin: true }))
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //connecting mongodb 
 
 mongoose.connect(DB)
-.then(()=>{
-    app.listen(port);
-    console.log(`connected to port : ${port}`);
-})
-.catch((err)=>{
-    console.log(err)
-});
+    .then(() => {
+        app.listen(port);
+        console.log(`connected to port : ${port}`);
+    })
+    .catch((err) => {
+        console.log(err)
+    });
 // app.listen(port)
 app.use(authRoutes)
 
